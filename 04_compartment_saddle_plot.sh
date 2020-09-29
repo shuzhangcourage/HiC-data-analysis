dcbffa7d5a8a511a72ab6eb3aad5cc9a17b21d5a
Shu Zhang <zhang.shu@stud.uni-goettingen.de>
2020-09-29 14:19:35 +0200

    Update 04_compartment_saddle_plot.sh

--- 04_compartment_saddle_plot.sh
+++ 04_compartment_saddle_plot.sh
@@ -10,18 +10,5 @@
 #SBATCH --mem=100gb
 #SBATCH --time=2:00:00
 
-# Commands
-# sbatch 04_compartment_saddle_plot.sh
-# squeue -u szhang3
-# scancel 
 
-# extracting matrix
-#time python /usr/users/szhang3/Project/Papantonis_Pol2Degron/Code/3_Annotation_pipline/04_compartment_saddle_plot.py reenter_merge_control reference 
-time python /usr/users/szhang3/Project/Papantonis_Pol2Degron/Code/3_Annotation_pipline/04_compartment_saddle_plot.py 14hG1_control non_reference
-time python /usr/users/szhang3/Project/Papantonis_Pol2Degron/Code/3_Annotation_pipline/04_compartment_saddle_plot.py 14hG1_degron non_reference
-time python /usr/users/szhang3/Project/Papantonis_Pol2Degron/Code/3_Annotation_pipline/04_compartment_saddle_plot.py 14hG1_Tri non_reference
-time python /usr/users/szhang3/Project/Papantonis_Pol2Degron/Code/3_Annotation_pipline/04_compartment_saddle_plot.py reenter_degron non_reference
-time python /usr/users/szhang3/Project/Papantonis_Pol2Degron/Code/3_Annotation_pipline/04_compartment_saddle_plot.py 2h_async_degron non_reference
-time python /usr/users/szhang3/Project/Papantonis_Pol2Degron/Code/3_Annotation_pipline/04_compartment_saddle_plot.py 2h_async_control non_reference
-
-
+#time python ./04_compartment_saddle_plot.py control reference 
