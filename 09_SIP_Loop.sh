be31f25b7e15de0736e964324c41fc248f7d8159
Shu Zhang <zhang.shu@stud.uni-goettingen.de>
2020-09-29 14:17:40 +0200

    Update 09_SIP_Loop.sh

--- 09_SIP_Loop.sh
+++ 09_SIP_Loop.sh
@@ -9,16 +9,11 @@
 #SBATCH --ntasks-per-node=1
 #SBATCH --mem=100gb
 #SBATCH --time=20:00:00
-#SBATCH --array=1-12
-
-# Commands
-# sbatch 01_SIP_Loop.sh
-# squeue -u szhang3
-# scancel 5129648
+#SBATCH --array=1-2
 
 # Input
-inputFileName="/usr/users/szhang3/Project/Papantonis_Pol2Degron/Code/3_Annotation_pipline/input/SIP_Loop_input_parameters.txt"
+inputFileName="./input/SIP_Loop_input_parameters.txt"
 parameters=`sed "${SLURM_ARRAY_TASK_ID}q;d" $inputFileName`
 
 # Detecting Loops
-time python /usr/users/szhang3/Project/Papantonis_Pol2Degron/Code/3_Annotation_pipline/03_SIP_Loop.py $parameters
+time python ./09_SIP_Loop.py $parameters
