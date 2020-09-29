88c4906541399170ea4061b106e56b4f5c756c37
Shu Zhang <zhang.shu@stud.uni-goettingen.de>
2020-09-29 14:23:38 +0200

    Update 03_interaction_decay.sh

--- 03_interaction_decay.sh
+++ 03_interaction_decay.sh
@@ -2,20 +2,13 @@
 
 # Slurm Parameters
 #SBATCH -p fat 
-#SBATCH --job-name=DI_TADs
-#SBATCH --output=DI_TADs.%A_%a.out
-#SBATCH --error=DI_TADs.%A_%a.err
+#SBATCH --job-name=interaction_decay
+#SBATCH --output=interaction_decay.%A_%a.out
+#SBATCH --error=interaction_decay.%A_%a.err
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=1
-#SBATCH --mem=100gb
+#SBATCH --mem=50gb
 #SBATCH --time=1:00:00
 
-# Commands
-# sbatch 04_DI_TADs.sh
-# squeue -u szhang3
-# scancel 
 
-
-# extracting matrix
 time python ./03_interaction_decay.py 10000 two dump/OB_10K/file1.xls dump/OB_10K/file2.xls
-
